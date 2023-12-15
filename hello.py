a39d2aa80268f11d1fd1668d54417752689bc736
lyubovchubarova <lyubachuba@gmail.com>
2023-12-15 15:03:51 +0300

    try commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+from typing import List, Dict, Tuple
+
+
+def get_array(array: list,
+              b: str,
+              ) -> List[int]:
+    '''
+    
+    '''
+    return [a for a in array if a % 2 == 0]
+
+
+class Example:
+    """
+    
+    """
+    pass
+
+
+
